91718c630b8525830bf7c1f7e82217a07f22c2de
yann.kervella.ext <yann.kervella.ext@oscaro.com>
2023-11-24 09:12:50 +0100

    Adding few stuff

--- app/app.py
+++ app/app.py
@@ -8,10 +8,20 @@ st.title('DEMO APP ESILV')
 
 
 display_page = st.sidebar.selectbox('Quelle page souhaitez-vous visiter ?',('HOMEPAGE', 'VISUALISATIONS', 'MACHINE LEARNING'))
+# HOMEPAGE STUFF
 if display_page == 'HOMEPAGE':
     homepage()
+    if st.button('Load Data'):
+        df_train = pd.read_csv('./data/Data_Train.csv')
+        st.write("Data Successfully loaded")
+        st.dataframe(data=df_train)
+        st.line_chart(data=df_train, x='Airline', y ='Price')
+# VISUALIZATION STUFF
 elif display_page == 'VISUALISATIONS':
     visualization()
+# MACHINE LEARNING STUFF
 elif display_page == 'MACHINE LEARNING':
     ml()
+
+
 
